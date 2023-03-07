584fdbe0963ba0c0064a6b84aa20cd749b00dcd4
devilzeros <devilzeros@outlook.co.th>
2023-03-07 17:55:17 +0700

    est tesy api types

---
+++ src/pages/api/types.ts
@@ -0,0 +1,82 @@
+
+// export interface WarnCheckEditLogData {
+//     id: string;
+//     title: string;
+//     tagLink: string;
+//     detail: string;
+//     firstname: string;
+//     lastname: string;
+//     memberId: string;
+//     workingsId: string;
+//     packagePriceId: string;
+//     newsId: string;
+//     aboutID: string;
+// }
+
+// export interface WarnAudienceRecordData {
+//     id: string;
+//     title: string;
+//     detail: string;
+// }
+
+export const contactData = [
+    { id: '1', title: 'c', detail: 'แก้ฟิล facebookแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipa จาก facebook.com/api เป็น facebook.com/ipa', firstname: 'งง', lastname: 'งง', memberId: '1', workingsId: '', packagePriceId: '', newsId: '', aboutID: '1' },
+    { id: '1', title: 'c', detail: 'แก้ฟิล facebookแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipa จาก facebook.com/api เป็น facebook.com/ipa', firstname: 'งง', lastname: 'งง', memberId: '1', workingsId: '', packagePriceId: '', newsId: '', aboutID: '1' },
+    { id: '1', title: 'c', detail: 'แก้ฟิล facebookแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipa จาก facebook.com/api เป็น facebook.com/ipa', firstname: 'งง', lastname: 'งง', memberId: '1', workingsId: '', packagePriceId: '', newsId: '', aboutID: '1' },
+    { id: '1', title: 'c', detail: 'แก้ฟิล facebookแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipa จาก facebook.com/api เป็น facebook.com/ipa', firstname: 'งง', lastname: 'งง', memberId: '1', workingsId: '', packagePriceId: '', newsId: '', aboutID: '1' },
+    { id: '1', title: 'c', detail: 'แก้ฟิล facebookแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipa จาก facebook.com/api เป็น facebook.com/ipa', firstname: 'งง', lastname: 'งง', memberId: '1', workingsId: '', packagePriceId: '', newsId: '', aboutID: '1' },
+    { id: '1', title: 'c', detail: 'แก้ฟิล facebookแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipa จาก facebook.com/api เป็น facebook.com/ipa', firstname: 'งง', lastname: 'งง', memberId: '1', workingsId: '', packagePriceId: '', newsId: '', aboutID: '1' },
+    { id: '1', title: 'c', detail: 'แก้ฟิล facebookแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipa จาก facebook.com/api เป็น facebook.com/ipa', firstname: 'งง', lastname: 'งง', memberId: '1', workingsId: '', packagePriceId: '', newsId: '', aboutID: '1' },
+    { id: '1', title: 'c', detail: 'แก้ฟิล facebookแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipaแก้ฟิล facebook จาก facebook.com/api เป็น facebook.com/ipa จาก facebook.com/api เป็น facebook.com/ipa', firstname: 'งง', lastname: 'งง', memberId: '1', workingsId: '', packagePriceId: '', newsId: '', aboutID: '1' },
+];
+
+export const warnCheckEditLogData = [
+    { id: '1', title: 'a', tagLink: 'news', detail: 'แก้ฟิล title จาก มองบน เป็น มองบนฟ้า', firstname: 'งง', lastname: 'งง', memberId: '1', workingsId: '1', packagePriceId: '', newsId: '', aboutID: '' },
+];
+
+export const warnAudienceRecordData = [
+    { id: '1', title: 'ผู้เข้าชมเว็บประจำวัน 3 พ.ค. 2565', detail: '185' },
+];
+
+
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+    id: string;
+    title: string;
+    detail: string;
+    firstname: string;
+    lastname: string;
+    memberId: string;
+    workingsId: string;
+    packagePriceId: string;
+    newsId: string;
+    aboutID: string;
+}
+type DataTwo = {
+    id: string;
+    title: string;
+    tagLink: string;
+    detail: string;
+    firstname: string;
+    lastname: string;
+    memberId: string;
+    workingsId: string;
+    packagePriceId: string;
+    newsId: string;
+    aboutID: string; 
+}
+type DataThree = {
+    id: string;
+    title: string;
+    detail: string;
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data[]>
+) {
+    res.status(200).json([...contactData])
+}
+
