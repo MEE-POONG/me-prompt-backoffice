b7f0a5599d77981d5327ed5144d97fcc8e066a3a
devilzeros <devilzeros@outlook.co.th>
2023-05-14 22:20:13 +0700

    est

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,6 @@ import React, { useEffect, useState } from "react";
 import Head from 'next/head';
 import LayOut from "@/components/LayOut";
 import HeroSection from '@/container/Home/HeroSection';
-import RequestSection from "@/container/Home/RequestSection";
-import WhySection from "@/container/Home/WhySection";
-import ServicesSection from "@/container/Home/ServicesSection";
-import GalleryList from "@/container/GalleryList";
 import { Alert, Card, Col, Row } from "react-bootstrap";
 
 
@@ -44,8 +40,14 @@ const HomePage: React.FC = () => {
               <Card.Header>
                 <h4 className="text-center">คิวงานบอท</h4>
               </Card.Header>
-              <Card.Body>
-                หห
+              <Card.Body className="y-scroll">
+                {Array.from({ length: 15 }, (_, index) => (
+                  <Alert key={index} variant='success'>
+                    <b>เติมเครดิต 9,000</b>
+                    <b>ยูส ufh27oa14</b>
+                    <b>14.00</b>
+                  </Alert>
+                ))}
               </Card.Body>
             </Card>
           </Col>
@@ -54,8 +56,14 @@ const HomePage: React.FC = () => {
               <Card.Header>
                 <h4 className="text-center">จัดการข้อมูล</h4>
               </Card.Header>
-              <Card.Body>
-                กก
+              <Card.Body className="y-scroll">
+                {Array.from({ length: 15 }, (_, index) => (
+                  <Alert key={index} variant='info'>
+                    <b>แก้ข้อมูล ufrcbaaa</b>
+                    <b>โดย Est</b>
+                    <b>14.00</b>
+                  </Alert>
+                ))}
               </Card.Body>
             </Card>
           </Col>
