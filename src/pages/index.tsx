63f984dbebf550cd5da554468b1e5cda4af92007
devilzeros <devilzeros@outlook.co.th>
2023-05-14 21:58:49 +0700

    est

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import RequestSection from "@/container/Home/RequestSection";
 import WhySection from "@/container/Home/WhySection";
 import ServicesSection from "@/container/Home/ServicesSection";
 import GalleryList from "@/container/GalleryList";
-import { Card, Col, Row } from "react-bootstrap";
+import { Alert, Card, Col, Row } from "react-bootstrap";
 
 
 const HomePage: React.FC = () => {
@@ -32,29 +32,49 @@ const HomePage: React.FC = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='home-page'>
-        {/* <Card>
+      <div className='home-page h-100'>
+        <Card className="title">
           <Card.Header>
             <h2 className="display-6 text-center">Welcome</h2>
           </Card.Header>
-        </Card> */}
-        <Row>
-          <Col lg="4">
-            <Card>
-              aa
+        </Card>
+        <Row className="warn">
+          <Col lg="4" className="h-100">
+            <Card className="h-100">
+              <Card.Header>
+                <h4 className="text-center">คิวงานบอท</h4>
+              </Card.Header>
+              <Card.Body>
+                หห
+              </Card.Body>
             </Card>
           </Col>
-          <Col lg="4">
-            <Card>
-              aa
+          <Col lg="4" className="h-100">
+            <Card className="h-100">
+              <Card.Header>
+                <h4 className="text-center">จัดการข้อมูล</h4>
+              </Card.Header>
+              <Card.Body>
+                กก
+              </Card.Body>
             </Card>
           </Col>
-          <Col lg="4">
-            <Card>
-              aa
+          <Col lg="4" className="h-100">
+            <Card className="h-100">
+              <Card.Header>
+                <h4 className="text-center">พันธมิตรเข้าใช้</h4>
+              </Card.Header>
+              <Card.Body className="y-scroll">
+                {Array.from({ length: 15 }, (_, index) => (
+                  <Alert key={index} variant='info'>
+                    This is a primary alert—check it out!
+                  </Alert>
+                ))}
+              </Card.Body>
             </Card>
           </Col>
         </Row>
+
       </div>
     </LayOut>
   );
