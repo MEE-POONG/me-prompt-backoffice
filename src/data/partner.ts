c6ff5ec4517d643acfac05e4a3cfa1a25aa1e021
devilzeros <devilzeros@outlook.co.th>
2023-08-10 15:43:34 +0700

    ำหะ

--- src/data/partner.ts
+++ src/data/partner.ts
@@ -1,10 +1,11 @@
 export const userAGForm = [
-    { title: "เลือกตำแหน่ง", labelShow: "UserAG / ยูสเซอร์AG", placeholderShow: "", typeShow: "dropdown", rules: 2, invalidFeedback: "กรอกข้อมูลอ้างอิงจากยูส", list: [{ text: "agent" }, { text: "master" }, { text: "senior" },] },
-    { title: "OriginAG", labelShow: "OriginAG / ยูสต้นสาย", placeholderShow: "name@example.com", typeShow: "text", rules: 2, invalidFeedback: "กรอก E-mail ไม่ถูกต้อง" },
-    { title: "เลือกตำแหน่ง", labelShow: "UserAG / ยูสเซอร์AG", placeholderShow: "", typeShow: "dropdown", rules: 2, invalidFeedback: "กรอกข้อมูลอ้างอิงจากยูส", list: [{ text: "agent" }, { text: "master" }, { text: "senior" },] },
+    { title: "positon", labelShow: "เลือกตำแหน่ง", placeholderShow: "กรุณาเลือก", typeShow: "dropdown", rules: 1, invalidFeedback: "ยังไม่ได้เลือกข้อมูล", list: [{ text: "agent" }, { text: "master" }, { text: "senior" },] },
+    { title: "originAG", labelShow: "OriginAG / ยูสต้นสาย", placeholderShow: "", typeShow: "search", rules: 2, invalidFeedback: "ระบบยูสต้นสาย" },
+
     { title: "userAG", labelShow: "UserAG / ยูสเซอร์AG", placeholderShow: "Enter name", typeShow: "text", rules: 2, invalidFeedback: "กรอกข้อมูลอ้างอิงจากยูส" },
+    { title: "เลือกตำแหน่ง", labelShow: "UserAG / ยูสเซอร์AG", placeholderShow: "", typeShow: "dropdown", rules: 2, invalidFeedback: "กรอกข้อมูลอ้างอิงจากยูส", list: [{ text: "agent" }, { text: "master" }, { text: "senior" },] },
     { title: "percen", labelShow: "เลือกเปอร์เซ็น", placeholderShow: "0.00 ถึง 0.40", typeShow: "select", rules: 2, invalidFeedback: "กรอกข้อมูลอ้างอิงจากยูส" },
-    { title: "recommender", labelShow: "Recommender / ผู้แนะนำ", placeholderShow: "เลือกยูสที่แนะนำ", typeShow: "text", rules: 2, invalidFeedback: "กรอกข้อมูล UserAG ผู้แนะนำ" },
+    { title: "recommender", labelShow: "Recommender / ผู้แนะนำ", placeholderShow: "เลือกยูสที่แนะนำ", typeShow: "search", rules: 2, invalidFeedback: "กรอกข้อมูล UserAG ผู้แนะนำ" },
 ];
 export const memberForm = [
     { id: "1", value: 'กสิกรไทย', image: 'https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/446b4b9c-af1e-47aa-e475-784f42d5a300/100' },
--- src/pages/partner/userAg/form.tsx
+++ src/pages/partner/userAg/form.tsx
@@ -1,8 +1,19 @@
 import BasicInput from "@/components/Input/Basic";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Card, Col, Form, Row } from "react-bootstrap";
 
 import { userAGForm } from "@/data/partner";
+import BasicDropdownInput from "@/components/Input/BasicDropdown";
+import BasicSearchInput from "@/components/Input/BasicSearch";
+import useAxios from "axios-hooks";
+import { Member } from "@prisma/client";
+
+interface Params {
+  page: number;
+  pageSize: number;
+  searchTerm: string;
+  totalPages: number;
+}
 const FormPage: React.FC = () => {
   const initialFormData = userAGForm.reduce((acc: any, curr: any) => {
     acc[curr.title] = '';
@@ -12,9 +23,29 @@ const FormPage: React.FC = () => {
   const [formData, setFormData] = useState(initialFormData);
   const [checkIsValid, setCheckIsValid] = useState<boolean>(false);
 
+  const [params, setParams] = useState<Params>({
+    page: 1,
+    pageSize: 10,
+    searchTerm: "",
+    totalPages: 1,
+  });
+  const [{ data: memberSearch, loading, error }, refetchSearch] = useAxios({
+    url: `/api/member?page=${params.page}&pageSize=${params.pageSize}&searchTerm=${formData["originAG"]}`,
+    method: "GET",
+  });
+
+  useEffect(() => {
+    if (formData["recommender"] && formData["recommender"].length >= 3) {
+      refetchSearch();
+    }
+    console.log(memberSearch);
+  }, [formData["originAG"], refetchSearch]);
+
+
   const handleInputChange = (field: string, value: string) => {
     setFormData((prev: Record<string, string>) => ({ ...prev, [field]: value }));
   };
+
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -29,19 +60,52 @@ const FormPage: React.FC = () => {
           <Form onSubmit={handleSubmit}>
             <Row>
               {userAGForm.map((inputItem, index) => (
-                <Col md={3} key={index}>
-                  <BasicInput
-                    title={inputItem.title}
-                    labelShow={inputItem.labelShow}
-                    placeholderShow={inputItem.placeholderShow}
-                    typeShow={inputItem.typeShow}
-                    valueShow={formData[inputItem.title]}
-                    valueSet={(value: any) => handleInputChange(inputItem.title, value)}
-                    rules={(value: any) => value.length >= 3}
-                    checkIsValid={checkIsValid}
-                    invalidFeedback={inputItem.invalidFeedback}
-                  />
+                <Col md={4} lg={3} key={index}>
+                  {inputItem.typeShow === "text" && (
+                    <BasicInput
+                      title={inputItem.title}
+                      labelShow={inputItem.labelShow}
+                      placeholderShow={inputItem.placeholderShow}
+                      typeShow={inputItem.typeShow}
+                      valueShow={formData[inputItem.title]}
+                      valueSet={(value: any) => handleInputChange(inputItem.title, value)}
+                      rules={(value: any) => value?.length >= 3}
+                      checkIsValid={checkIsValid}
+                      invalidFeedback={inputItem.invalidFeedback}
+                    />
+                  )}
 
+                  {inputItem.typeShow === "dropdown" && (
+                    <BasicDropdownInput
+                      title={inputItem.title}
+                      labelShow={inputItem.labelShow}
+                      placeholderShow={inputItem.placeholderShow}
+                      typeShow={inputItem.typeShow}
+                      valueSet={(value: string) => handleInputChange(inputItem.title, value)}
+                      rules={(value: string) => value?.length >= 3}
+                      checkIsValid={checkIsValid}
+                      invalidFeedback={inputItem.invalidFeedback}
+                      list={inputItem.list || []}
+                    />
+                  )}
+
+                  {inputItem.typeShow === "search" && (
+                    <BasicSearchInput
+                      title={inputItem.title}
+                      labelShow={inputItem.labelShow}
+                      placeholderShow={inputItem.placeholderShow}
+                      typeShow={inputItem.typeShow}
+                      valueShow={formData[inputItem.title]}
+                      valueSet={(value: any) => handleInputChange(inputItem.title, value)}
+                      rules={(value: any) => value?.length >= 3}
+                      checkIsValid={checkIsValid}
+                      invalidFeedback={inputItem.invalidFeedback}
+                      listArray={memberSearch?.data.map((member: any) => ({
+                        id: member.id,
+                        textShow: member?.firstname + " " + member?.lastname
+                      }))}
+                    />
+                  )}
                 </Col>
               ))}
               <Col sm="12">
