74c03ec8c0403d616dac28e0b83d5d4691c755a8
devilzeros <devilzeros@outlook.co.th>
2023-09-07 10:21:02 +0700

    est

--- src/pages/api/botAG/index.ts
+++ src/pages/api/botAG/index.ts
@@ -18,22 +18,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         case 'POST':
             try {
-                const { title, username, position, formDate, toDate /* add your new fields here */ } = req.body;
-            
+                const { title, username, position, formDate, toDate } = req.body;
                 const newQueueBot = await prisma.queueBot.create({
                     data: {
-                        title,
-                        username: username ?? '',
-                        position: position ?? '',
-                        formDate: formDate ? new Date(formDate) : null,
-                        toDate: toDate ? new Date(toDate) : null,
-                        // add your new fields here
+                        title,  // make sure this corresponds to your Prisma model
+                        username,
+                        position,
+                        formDate: new Date(formDate) ?? null, // Converting to Date object
+                        toDate: new Date(toDate) ?? null,  
                     },
                 });
-            
+
                 res.status(201).json({ success: true, data: newQueueBot });
             } catch (error) {
-                res.status(500).json({ success: true, message: "An error occurred while creating the queueBot" });
+                res.status(500).json({ success: true, message: "An error occurred while creating the member" });
             }
             break;
 
